2759dd088afb7cf43477672b86934444ffa12567
eatmorecellphone <37182003+eatmorecellphone@users.noreply.github.com>
2018-03-09 15:19:09 +0800

    Update 1_two_sum.py

--- 1_two_sum.py
+++ 1_two_sum.py
@@ -1,1 +1,8 @@
-
+class Solution:
+    def twoSums(self,nums,target):
+        map={}
+        for i,num in enumerate(nums):
+            if target-num in map:
+                return(map[target-num]+1,i+1)
+            d[num]=i
+            
